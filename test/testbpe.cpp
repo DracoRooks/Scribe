f88c6e0a5c48d48e57bc5d2bb3a01b95c447a1d2
DracoRooks <gokulmittal008@gmail.com>
2026-07-07 17:02:21 +0530

    add: Added a bare-bones test for BPE.

---
+++ test/testbpe.cpp
@@ -0,0 +1,17 @@
+#include "../include/byte_pair_encoder.hpp"
+
+#include <iostream>
+
+int main() {
+    Scribe::BytePairEncoder bpe;
+    bpe.train("./test/testdata.txt", 34, false);
+
+    std::cout << "Sample Data: Hello! testing... 🫡" << std::endl;
+    const std::vector<int> encodedData = bpe.encode("Hello! testing... 🫡");
+    std::cout << "Encoded Data: [";
+    for (const auto token : encodedData) std::cout << token << ", ";
+    std::cout << "]" << std::endl;
+    const std::vector<u_int8_t> decodedData = bpe.decode(encodedData);
+    const std::string str(decodedData.begin(), decodedData.end());
+    std::cout << "Decoded Data: '" << str << "'" << std::endl;
+}
